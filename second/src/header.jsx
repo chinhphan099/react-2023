c2a19b57fe401de651df8f5c4aa35c8190490718
Chinh Phan <chinhphan099@gmail.com>
2023-03-22 16:57:06 -0400

    no message

---
+++ second/src/header.jsx
@@ -0,0 +1,6 @@
+const firstName = 'Chinh'
+const sum = (a, b) => a + b
+const elm1 = <h1>My name is {firstName}. I'm {sum(10, 23)}</h1>
+const elm2 = <a href="google.com" className="link">Google</a>
+const h3 = <h3 />
+const input = <input name="lastName" placeholder="abc" />
