ff869e899c03e756891cc147b8e1e83402bc574f
Chinh Phan <chinhphan099@gmail.com>
2023-05-04 10:20:50 -0400

    clone project using createAction, createReducer to use createSlide

---
+++ local-blog/src/pages/blog/blog.reducer.ts
@@ -0,0 +1,58 @@
+import { createAction, createReducer } from '@reduxjs/toolkit'
+import { initalPostList } from 'constant/blog'
+import { Post } from 'types/blog.type'
+
+interface BlogState {
+  postList: Post[]
+  editingPost: Post | null
+}
+const initalState: BlogState = {
+  postList: initalPostList,
+  editingPost: null
+}
+
+export const addPost = createAction<Post>('blog/addPost')
+export const deletePost = createAction<string>('blog/deletePost')
+export const startEditingPost = createAction<string>('blog/startEditingPost')
+export const finishEditingPost = createAction<Post>('blog/finishEditingPost')
+export const cancelEditingPost = createAction('blog/cancelEditingPost')
+
+const blogReducer = createReducer(initalState, (builder) => {
+  builder
+    .addCase(addPost, (state, action) => {
+      const post = action.payload
+      state.postList.push(post)
+    })
+    .addCase(deletePost, (state, action) => {
+      const id = action.payload
+      const foundPostIndex = state.postList.findIndex((post) => post.id === id)
+      const foundPost = state.postList.find((post) => post.id === id) || null
+      if (foundPostIndex !== -1 && foundPost) {
+        state.postList.splice(foundPostIndex, 1)
+        if (state.editingPost && state.editingPost.id === foundPost.id) {
+          state.editingPost = null
+        }
+      }
+    })
+    .addCase(startEditingPost, (state, action) => {
+      const id = action.payload
+      const foundPost = state.postList.find((post) => post.id === id) || null
+      state.editingPost = foundPost
+    })
+    .addCase(finishEditingPost, (state, action) => {
+      const postId = action.payload.id
+      state.postList.some((post, index) => {
+        if (post.id === postId) {
+          state.postList[index] = action.payload
+          return true
+        }
+        return false
+      })
+      state.editingPost = null
+    })
+    .addCase(cancelEditingPost, (state) => {
+      state.editingPost = null
+    })
+})
+
+export default blogReducer
